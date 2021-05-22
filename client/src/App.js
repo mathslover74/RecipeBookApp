895bab4f7057fd43898c9bd5bd2a080e44b9d919
mathslover74 <Jason.chng.sy@gmail.com>
2021-05-22 11:01:33 +0800

    login all working with logout function and login with in session

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 // import logo from './logo.svg';
-import React ,{ useState }from 'react';
-import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom'
-import Routes from './routes/routes'
-import AuthApi from './utils/AuthAPI'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes/routes";
+import AuthApi from "./utils/AuthAPI";
+import { hasSignned } from "./components/auth-api";
 
 function App() {
-  const [auth,setAuth] = useState(false);
+  const [auth, setAuth] = useState(false);
+
+  const readSession = async () => {
+    const result = await hasSignned();
+    console.log(result);
+    if (result.data.auth) {
+      setAuth(true);
+    }
+  };
+  useEffect(() => {
+    readSession();
+  }, []);
   return (
     <div className="App">
       <AuthApi.Provider value={{ auth, setAuth }}>
-      <Router>
-        <Routes />
-      </Router>
+        <Router>
+          <Routes />
+        </Router>
       </AuthApi.Provider>
     </div>
   );
