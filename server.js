b9ba58ab7f505aaa4e96609b9f0fab45997d3480
mathslover74 <Jason.chng.sy@gmail.com>
2021-06-10 20:30:58 +0800

    try 3

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const methodOverride = require("method-override");
 const app = express();
 const session = require("express-session");
 const db = mongoose.connection;
-const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 require("dotenv").config();
-const users = [];
 
 // Enviroment variables
 const mongoURI = process.env.MONGODB_URI;
@@ -17,11 +16,10 @@ const path = require("path");
 const usersController = require("./controllers/users");
 const recipesController = require("./controllers/recipes");
 
-const bodyParser = require("body-parser");
-
 // Middleware
 // allows us to use put and delete methods
-app.use(express.static(path.join("client/build")));
+// app.use(express.static(path.join("client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 // app.use(express.static(__dirname + "/public"));
 
 app.use(methodOverride("_method"));
